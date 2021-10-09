ad29bbfcb6f67a8856816b6d55bcf8d58ea287b2
Benjamin Netanyahu <beniiboii00@gmail.com>
2021-10-09 19:55:21 +1100

    Update convertHandler.js

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,66 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
-    
-    return result;
+    const err = 'invalid number';
+    const num = input.toLowerCase().split(/[a-z]/gi)[0]; 
+    const hasToEval = num.match(/\//gi);
+    if (hasToEval && hasToEval.length === 1){ return eval(num); }
+    if (isNaN(num)){ return err; }
+    if (num === '') { return 1; }
+    return parseFloat(num);
   };
   
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
+ this.getUnit = function(input) {
+    const err = 'invalid unit';
+    const supportedUnits = ["gal", "l", "mi", "km", "lbs", "kg"];
+    const unit = supportedUnits.includes(input.toLowerCase()) ? input.toLowerCase() : input.toLowerCase().split(/[^a-z]+/gi)[1];
+    const res = supportedUnits.includes(unit) ? unit : err;
+    return res === 'l' ? res.toUpperCase() : res;
   };
   
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+  
+   this.getReturnUnit = function(initUnit) {
+    const units = {
+      "gal": "L",
+      "l": "gal",
+      "mi": "km",
+      "km": "mi",
+      "lbs": "kg",
+      "kg": "lbs"
+    };
+    return units[initUnit.toLowerCase()];
   };
 
   this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+    const unitsString = {
+      "gal": "gallon",
+      "l": "liters",
+      "mi": "miles",
+      "km": "kilometers",
+      "lbs": "pounds",
+      "kg": "kilograms"
+    };
+    return unitsString[unit.toLowerCase()];
   };
   
   this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
+    const num = initNum;
+    const refUnits = {
+      "gal": num * 3.78541,
+      "l": num / 3.78541,
+      "mi": num * 1.60934,
+      "km": num / 1.60934,
+      "lbs": num * 0.453592,
+      "kg": num / 0.453592
+    };
+    return parseFloat(refUnits[initUnit.toLowerCase()].toFixed(5));
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    return `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}` 
   };
   
 }
 
 module.exports = ConvertHandler;
+
